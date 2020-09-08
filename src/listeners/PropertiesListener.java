19fbfc914b020f9866d13d1842fdf10ce06cfa26
takuya.sudou <iverson232401@gmail.com>
2020-09-08 22:52:44 +0900

    Add Employee create

--- src/listeners/PropertiesListener.java
+++ src/listeners/PropertiesListener.java
@@ -1,5 +1,13 @@
 package listeners;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -30,6 +38,22 @@ public class PropertiesListener implements ServletContextListener {
      */
     public void contextInitialized(ServletContextEvent arg0)  { 
          // TODO Auto-generated method stub
+        ServletContext context = arg0.getServletContext();
+
+        String path = context.getRealPath("/META-INF/application.properties");
+        try {
+            InputStream is = new FileInputStream(path);
+            Properties properties = new Properties();
+            properties.load(is);
+            is.close();
+
+            Iterator<String> pit = properties.stringPropertyNames().iterator();
+            while(pit.hasNext()) {
+                String pname = pit.next();
+                context.setAttribute(pname, properties.getProperty(pname));
+            }
+        } catch(FileNotFoundException e) {
+        } catch(IOException e) {}
     }
 	
 }
